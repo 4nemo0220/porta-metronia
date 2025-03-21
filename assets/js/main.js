40b2ba814f45c16605439188bb81c78fd005eb06
Chris Salvatori <cro.salvatori@gmail.com>
2025-03-21 18:59:23 +0100

    changed servizi and menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,17 @@
 // main.js
 // Al momento non sono presenti funzionalità custom, ma potrai aggiungere qui il tuo codice JS.
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
